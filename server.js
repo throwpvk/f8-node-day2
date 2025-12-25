987e3f18bf53b03cfeddf7de4739d265c47c60f0
Phan Van Khai <215381564+throwpvk@users.noreply.github.com>
2025-12-25 09:59:18 +0900

    fix base api & allow render page origin

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(
       "http://localhost:5173",
       "https://throwpvk.github.io",
       "http://127.0.0.1:5500",
+      "https://feed-6z7f.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     optionsSuccessStatus: 200,
